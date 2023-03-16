eeb9927ede1b8819687bf454f2cd01b539382218
Tanzzim <tanzim.ahmed.20@ucl.ac.uk>
2023-03-16 21:32:19 +0000

    cleaning up

--- house_price_app/data/create_ml_model.py
+++ house_price_app/data/create_ml_model.py
@@ -1,11 +1,7 @@
 from pathlib import Path
 import pandas as pd
-import numpy as np
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 import pickle
 
 
